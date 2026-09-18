7fc1de215ddc20cb5b6b800e8c5c549338ba32c5
Michael Jones <michael.jones90@me.com>
2026-09-18 10:47:10 +0100

    Initial Commit

---
+++ HackingWithSwift-HighRollers/HackingWithSwift_HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwift_HighRollersApp.swift
+//  HackingWithSwift-HighRollers
+//
+//  Created by Michael Jones on 18/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwift_HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
