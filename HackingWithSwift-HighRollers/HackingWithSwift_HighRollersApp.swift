34ed0bc1eddb27e10a32ebeae0c0fc45ca437c0c
Michael Jones <michael.jones90@me.com>
2026-09-19 09:27:46 +0100

    added some template code to handle SwiftData

--- HackingWithSwift-HighRollers/HackingWithSwift_HighRollersApp.swift
+++ HackingWithSwift-HighRollers/HackingWithSwift_HighRollersApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct HackingWithSwift_HighRollersApp: App {
@@ -13,5 +14,6 @@ struct HackingWithSwift_HighRollersApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Dice.self)
     }
 }
